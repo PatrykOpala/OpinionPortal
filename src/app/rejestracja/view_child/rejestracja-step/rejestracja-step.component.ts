bcc6d7e74e58849fca05ea4ad31e04895db731dd
Patryk Opala <patrykopala265@gamil.com>
2022-10-29 11:32:59 +0200

    0.0.47

--- src/app/rejestracja/view_child/rejestracja-step/rejestracja-step.component.ts
+++ src/app/rejestracja/view_child/rejestracja-step/rejestracja-step.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'opn-rejestracja-step',
@@ -8,20 +6,7 @@ import { Observable, switchMap } from 'rxjs';
   styleUrls: ['./rejestracja-step.component.scss']
 })
 export class RejestracjaStepComponent implements OnInit {
-  placeholder = 0;
-  placeholder$?: Observable<string>;
-  accountLabel: string = "";
-  constructor(private rejestracjaRoute: ActivatedRoute, private nextRouter: Router) { }
-  ngOnInit(): void {
-   this.placeholder$ = this.rejestracjaRoute.paramMap.pipe(switchMap((param: ParamMap)=> param.get('idstep')!));
-   this.placeholder$.subscribe(r => {
-    if(Number(r) === 1) this.placeholder = 0;
-    if(Number(r) === 2) this.placeholder = 1;
-    if(Number(r) > 2) this.nextRouter.navigateByUrl("/");
-   })
-  }
-  setAccountLabel(event: string){
-    this.accountLabel = event;
-    this.nextRouter.navigateByUrl("rejestracja/step/2");
-  }
+  @Input() step!: number;
+  constructor() { }
+  ngOnInit(): void {}
 }
