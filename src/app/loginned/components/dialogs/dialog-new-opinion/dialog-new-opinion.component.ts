d467a0301d46602ce4999713d9b48b67b0e4ded9
Patryk Opala <patrykopala265@gamil.com>
2023-02-25 11:29:50 +0100

    [0.0.70 dev] Prototype dialog-new-opinion v3

--- src/app/loginned/components/dialogs/dialog-new-opinion/dialog-new-opinion.component.ts
+++ src/app/loginned/components/dialogs/dialog-new-opinion/dialog-new-opinion.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { getDataFromLocalStorage } from 'src/app/core/shared/utils/ts/localStorage.functions';
+import { LOCAL_STORAGE_KEYS } from 'src/app/core/types/constants';
+import { CreateOpinion } from 'src/app/core/types/functions';
+import { Opinions, SupabaseUser } from 'src/app/core/types/interfaces';
 
 @Component({
   selector: 'opn-dialog-new-opinion',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./dialog-new-opinion.component.scss']
 })
 export class DialogNewOpinionComponent {
+  @ViewChild('area') textAreaElement!: ElementRef;
+  @ViewChild('actionButton') actionButton!: ElementRef;
+  @Output() opinionEmitter = new EventEmitter<Opinions>();
+  protected valu = "";
+  protected _ViewSelected = 0;
 
+  backView(){
+    if(this._ViewSelected === 1){
+      this._ViewSelected = 0;
+      this.actionButton.nativeElement.textContent = "Dalej";
+    }
+  }
+
+  closeDialog(){
+    
+  }
+
+  toogle(e: Event){
+    if((e.target as HTMLElement).localName !== "div"){
+      this.valu = (e.target as HTMLElement).textContent as string;
+      this._ViewSelected = 1;
+      this.actionButton.nativeElement.textContent = "Wystaw opinię";
+    }
+  }
+
+  giveFeedback(){
+    const user = getDataFromLocalStorage<SupabaseUser>(LOCAL_STORAGE_KEYS.userAuthentication).user; // TEMPORARY:TODO:Reminder change it!
+    let newOpinionObj: Opinions = CreateOpinion(user.id, user.email as string, Math.floor(Math.random() * 1000), this.valu, this.textAreaElement.nativeElement.value);
+    this.opinionEmitter.emit(newOpinionObj);
+    this.textAreaElement.nativeElement.value = "";
+    this._ViewSelected = 100;
+  }
 }
