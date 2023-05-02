28e070d11577bdee137a80ca7c7599dcaf203184
Patryk Opala <patrykopala265@gamil.com>
2023-05-02 13:26:25 +0200

    init build commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://fgyxvsnidcajiwhotsmu.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZneXh2c25pZGNhaml3aG90c211Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjEyNDM5NTYsImV4cCI6MTk3NjgxOTk1Nn0.r-CNl83camov8XmqAqqid_RGHOKJ5Wy317mxYTHvvRw'
 };
