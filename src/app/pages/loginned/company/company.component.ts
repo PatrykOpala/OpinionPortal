0341931995c78965fe3293de4fa7b8fb95c80dd7
Patryk Opala <patrykopala265@gamil.com>
2023-05-07 11:47:55 +0200

    add inside to 'Company Page'

--- src/app/pages/loginned/company/company.component.ts
+++ src/app/pages/loginned/company/company.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { DialogProduktService } from 'src/app/core/services/dialog/dialog-produkt/dialog-produkt.service';
+import { ProductService } from 'src/app/core/services/product/product.service';
+import { UserStoreAbstract } from 'src/app/core/types/classes/user-store-abstract.class';
+import { DialogAddProduktAndServiceComponent } from '../components/dialogs/dialog-add-produkt-and-service/dialog-add-produkt-and-service.component';
 
 @Component({
   selector: 'opn-company',
   templateUrl: './company.component.html',
   styleUrls: ['./company.component.scss']
 })
-export class CompanyComponent {
+export class CompanyComponent extends UserStoreAbstract implements OnInit{
+  protected dialogProduktService = inject(DialogProduktService);
+  protected authService = inject(AuthService);
+  protected productService = inject(ProductService);
 
+  constructor(public matDialog: MatDialog){
+    super()
+  }
+  ngOnInit(): void {
+    this.productService.getProductsFromDatabase();
+  }
+
+  openProduktServiceDialog(){
+    this.matDialog.open(DialogAddProduktAndServiceComponent);
+  }
+
+  deleteProductFromDatabasee(getId: number){
+    this.productService.deleteProductFromDatabase(this.productService.
+      product.filter(x => x.id === getId)[0]);
+  }
 }
